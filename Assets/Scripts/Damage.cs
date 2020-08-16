251ad544fc39c447c39aa8b308b548b9ae0eb88f
Adrian Martinez <adrianjustinmartinez@gmail.com>
2020-08-15 21:44:04 -0700

    Add in damage / damageables

---
+++ Assets/Scripts/Damage.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage : MonoBehaviour
+{
+    [Header("Options")]
+    [Tooltip("Controls total damage done")]
+    public float Dmg = 1;
+
+    public virtual void DealDamage(Damageable obj)
+    {
+        obj.OnDamage(Dmg);
+    }
+}
