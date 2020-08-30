b72dec0a756acb9f845d76c9ba3f833d19cb7dc5
Adrian Martinez <adrianjustinmartinez@gmail.com>
2020-08-30 13:38:42 -0700

    Actual smacks

--- Assets/Scripts/TriggerDamage.cs
+++ Assets/Scripts/TriggerDamage.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class TriggerDamage : Damage
 {
     private Collider m_Collider;
+    private GameObject m_otherObj;
     private void Awake()
     {
         m_Collider = GetComponent<Collider>();
@@ -13,14 +14,23 @@ public class TriggerDamage : Damage
             m_Collider.isTrigger = true;
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        Debug.Log(this + " collided with : " + other.gameObject);
-    }
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    Debug.Log(this + " collided with : " + other.gameObject);
+    //    m_otherObj = other.gameObject;
+    //}
+
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    m_otherObj = null;
+    //}
 
     public override void DealDamage(Damageable obj)
     {
+        //if (!m_otherObj) return;
+        obj.OnDamage(Dmg);
         //Debug.Log("Dealing " + (m_DmgMultiplier * Dmg) + " damage to " + obj);
         //obj.OnDamage(Dmg * m_DmgMultiplier);
+        Debug.Log("Dealing " + Dmg + " damage to : " + obj + "\nAt hp : " + obj.Health);
     }
 }
