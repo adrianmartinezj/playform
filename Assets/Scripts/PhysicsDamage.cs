29839ac66d22570072dd1605cd109f459e0398b7
Adrian Martinez <adrianjustinmartinez@gmail.com>
2020-09-02 08:03:09 -0700

    Round damage to deal with nicer info

--- Assets/Scripts/PhysicsDamage.cs
+++ Assets/Scripts/PhysicsDamage.cs
@@ -19,7 +19,14 @@ public class PhysicsDamage : Damage
 
     public override void DealDamage(Damageable obj)
     {
-        Debug.Log("Dealing " + (m_DmgMultiplier * Dmg) + " damage to " + obj);
-        obj.OnDamage(Dmg * m_DmgMultiplier);
+        float potentialDmg = m_DmgMultiplier * Dmg;
+        // Round damage to two decimals to prevent this from going cray cray
+        float temp = Mathf.Floor(potentialDmg) + (Mathf.Floor((potentialDmg - Mathf.Floor(potentialDmg)) * 100) / 100);
+        potentialDmg = temp;
+        Debug.Log("Dealing " + potentialDmg + " damage to " + obj);
+
+        // Let's not deal with super small damage...
+        if (potentialDmg < .01) return;
+        obj.OnDamage(potentialDmg);
     }
 }
